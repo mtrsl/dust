3823fc792554ebd2894cfe0a0ab38d26fb1c5cec
Matthew Russell <matthewjohnrussell@gmail.com>
2025-11-22 17:50:31 +0000

    comment to remind about rng_state -> ctr

--- inst/include/dust/particle.hpp
+++ inst/include/dust/particle.hpp
@@ -23,6 +23,8 @@ public:
     y_swap_(model_.size()) {
   }
 
+  // TODO(mjr) Need to add rng ctr arg (or some indices from which it can be
+  // calculated in e.g. update()) to replace rng_state
   void run(const time_type time_end) {
     while (time_ < time_end) {
       model_.update(time_, y_.data(), y_swap_.data());
