299bb79696d6de00f52df43182cfcc43f1a3a3da
Matthew Russell <matthewjohnrussell@gmail.com>
2026-01-21 16:56:05 +0000

    Potentially working buffered philox generator
    Needs a lot of actual testing but it seems to draw different blocks for
    each (ctr, key) pair and the numbers generated seem sensible at first
    glance

--- inst/include/dust/random/philox.hpp
+++ inst/include/dust/random/philox.hpp
@@ -3,8 +3,64 @@
 
 #include "dust/random/cuda_compatibility.hpp"
 
+#include <cstdio>
+
 namespace dust {
 namespace random {
+
+// Multiplies two 32 bit unsigned ints and keeps only the lowest 32 bits of the result
+__host__ __device__
+uint32_t umullo_u32(uint32_t a, uint32_t b) {
+#ifdef __CUDA_ARCH__
+    return a * b;
+#else
+    return static_cast<uint32_t>(a * b);
+#endif
+}
+
+// Multiplies two 32 bit unsigned ints and keeps only the highest 32 bits of the result
+__host__ __device__
+uint32_t umulhi_u32(uint32_t a, uint32_t b) {
+#ifdef __CUDA_ARCH__
+    return __umulhi(a, b);
+#else
+    return static_cast<uint32_t>((static_cast<uint64_t>(a) * static_cast<uint64_t>(b)) >> 32);
+#endif
+}
+
+// Helper types to make it easier to pass arrays by value (because arrays decay
+// to pointers when passed to functions)
+struct u32x2 {
+  uint32_t d[2];
+
+  // Element access (const)
+  __host__ __device__
+  inline const uint32_t& operator[](int i) const {
+    return d[i];
+  }
+
+  // Element access (mutable)
+  __host__ __device__
+  inline uint32_t& operator[](int i) {
+    return d[i];
+  }
+};
+
+struct u32x4 {
+  uint32_t d[4];
+
+  // Element access (const)
+  __host__ __device__
+  inline const uint32_t& operator[](int i) const {
+    return d[i];
+  }
+
+  // Element access (mutable)
+  __host__ __device__
+  inline uint32_t& operator[](int i) {
+    return d[i];
+  }
+};
 
 // The Philox counter type, analogous to the "state" in a traditional RNG -
 // passed to RNG fns etc - but the counter doesn't need to be stored (more than
@@ -18,15 +74,16 @@ public:
   using int_type = uint32_t;
 
   /// Static method, returning the number of integers per state
-  __host__ __device__ static constexpr size_t size() {
+  //__host__ __device__
+  static constexpr size_t size() {
     return 4;
   }
 
   /// Counter
-  int_type ctr[4];
+  u32x4 ctr = { 0, 0, 0, 0 };
 
   /// Key
-  int_type key[2];
+  u32x2 key = { 0, 0 };
 
   /// This flag indicates that the distributions should return the
   /// deterministic expectation of the draw, and not use any random
@@ -34,37 +91,85 @@ public:
   bool deterministic = false;
 
   /// Accessor method, used to both get and set the underlying state
-  __host__ __device__ int_type& operator[](size_t i) {
+  __host__ __device__
+  int_type& operator[](size_t i) {
     return ctr[i];
   }
+
+ __host__ __device__
+  uint32_t next_u32() {
+    if (idx == 4) {
+      buf = generate_block(ctr, key);
+
+      //printf("Random block drawn: (%u, %u, %u, %u)\n", buf[0], buf[1], buf[2], buf[3]);
+
+      ctr[3] += 1;
+      idx = 0;
+    }
+    const uint32_t result = buf[idx];
+    idx += 1;
+    return result;
+  }
+
+private:
+  static const size_t ROUNDS = 10;
+
+  static const uint32_t M0 = 0xD2511F53;
+  static const uint32_t M1 = 0xCD9E8D57;
+
+  // Weyl constants
+  static const uint32_t W0 = 0x9E3779B9;
+  static const uint32_t W1 = 0xBB67AE85;
+
+  int idx = 4;
+  u32x4 buf;
+
+  /// Generates the next block of 4 u32s in the stream. `ctr` and `key` copied
+  /// into the function arguments and local copies are modified by this
+  /// function - class members left unchanged
+  __host__ __device__
+  static u32x4 generate_block(
+    u32x4 ctr,
+    u32x2 key
+  ) {
+    for (size_t i = 0; i < ROUNDS; i += 1) {
+      uint32_t hi0 = umulhi_u32(ctr[0], M0);
+      uint32_t lo0 = umullo_u32(ctr[0], M0);
+
+      uint32_t hi1 = umulhi_u32(ctr[2], M1);
+      uint32_t lo1 = umullo_u32(ctr[2], M1);
+
+      u32x4 ctr_new = {
+        hi1 ^ ctr[1] ^ key[0],
+        lo1,
+        hi0 ^ ctr[3] ^ key[1],
+        lo0
+      };
+
+      ctr = ctr_new;
+
+      key[0] += W0;
+      key[1] += W1;
+    }
+
+    return ctr;
+  }
 };
-
-// Implementing these causes lots of duplicate definition errors, and
-// commenting them out makes it compile fine (though while still using xoshiro)
-
-//bool operator==(const philox4x32_10& lhs, const philox4x32_10& rhs) {
-  //return lhs.deterministic == rhs.deterministic &&
-    //std::equal(std::begin(lhs.state), std::end(lhs.state),
-               //std::begin(rhs.state));
-//}
-
-//bool operator!=(const philox4x32_10& lhs, const philox4x32_10& rhs) {
-  //return !(lhs == rhs);
-//}
-
 
 // Implementation of RNG interface functions
 
 template <>
-inline __host__ __device__ uint32_t next(philox4x32_10& state) {
-  // TODO(mjr) this is currently a dummy next fn that always returns 123 and
-  // the proper one needs to be implemented
-  //
-  // TODO(mjr) Philox4x32-10 generates blocks of 4 u32s at a time. How to
-  // handle this? At first just return the first element of a block and ignore
-  // the other 3.
-  state[3] += 1;
-  return 123;
+inline
+__host__ __device__
+uint32_t next(philox4x32_10& state) {
+  //printf("    ctr: (%u, %u, %u, %u)", state.ctr[0], state.ctr[1], state.ctr[2], state.ctr[3]);
+  //printf("    key: (%u, %u)", state.key[0], state.key[1]);
+  //printf("\n");
+
+  uint32_t n = state.next_u32();
+  //printf("Random u32 drawn: %u", n);
+
+  return n;
 }
 
 
